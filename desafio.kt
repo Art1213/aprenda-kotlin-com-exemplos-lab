9889b9d948817709613a8124097880c3e976180e
Art1213 <143416604+Art1213@users.noreply.github.com>
2024-01-31 19:43:35 -0300

    Update desafio.kt
    Resolução do desafio em Kotlin.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,30 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class ConteudoEducacional(var nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Usuario(var nome: String, val matricula: String, var formacao: Formacao, var conteudos: List<ConteudoEducacional>)
+
+class Matricula {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(nome: String, matricula: String, formacao: Formacao, conteudos: List<ConteudoEducacional>): Usuario {
+        val usuario = Usuario(nome, matricula, formacao, conteudos)
+        inscritos.add(usuario)
+        return usuario
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val formacao = Formacao("Back-End", listOf(ConteudoEducacional("Python", 60), ConteudoEducacional("Java", 45)))
+    val usuarioCadastrado = Matricula().matricular("Artur", "04112515", formacao, formacao.conteudos)
+
+    println("${usuarioCadastrado.nome}, ${usuarioCadastrado.matricula}, ${usuarioCadastrado.formacao.nome}")
+
+    usuarioCadastrado.conteudos.forEach {
+        println("Conteúdo: ${it.nome}, ${it.duracao} horas")
+    }
 }
